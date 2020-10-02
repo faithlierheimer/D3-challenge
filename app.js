0fce8738d36ddb41e3fd5e2ff946941727815f47
faithlierheimer <flierheimer@gmail.com>
2020-10-02 12:14:44 -0600

    setting up initial chart dimensions and such

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 //main goal: make a scatter plot that
 //explores healthcare vs. poverty or smokers vs. age
 
-//step 1: read in data.csv file 
+//step 1: define SVG area dimensions
+//REASON TO DO OTHER SIZES? -Nelson Q
+var svgWidth = 960;
+var svgHeight = 660;
 
-d3.csv("data/data.csv").then(function(healthData){
+//define chart margins--again, reason to do other measurements?
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+//use above measurements to define chart area 
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight = chartMargin.top - chartMargin.bottom;
+
+//select scatter div (shouldn't be body like in examples, right?)
+//as svg, set dimensions
+var svg = d3
+          .select("body")
+          .append("svg")
+          .attr("height", svgHeight)
+          .attr("width", svgWidth);
+
+//Add a chart group to svg area, translate it to be within the 
+//predefined chart margins
+
+var chartGroup = svg.append("g")
+                 .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+d3.csv("data.csv").then(function(healthData){
     //print data to check it
     console.log(healthData);
 
