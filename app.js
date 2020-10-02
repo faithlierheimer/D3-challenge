c8e80705d093974931d7d859aaaae2c342abce4e
faithlierheimer <flierheimer@gmail.com>
2020-10-02 11:30:53 -0600

    initial pseudocode and file structure

---
+++ app.js
@@ -0,0 +1,26 @@
+//main goal: make a scatter plot that
+//explores healthcare vs. poverty or smokers vs. age
+
+//step 1: read in data.csv file 
+
+d3.csv("data/data.csv").then(function(healthData){
+    //print data to check it
+    console.log(healthData);
+
+    //cast relevant parts of data to own arrays
+
+    //select scatter div to append svg
+
+    //code circles
+
+    //bind data to circles for scatterplot
+
+    //code state abbrevs to each circle
+
+
+    //code individual axes
+
+    //scale axes 
+
+    //populate axes w/numbers 
+})
